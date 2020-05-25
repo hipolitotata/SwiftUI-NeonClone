c692e4ec5efafb81685ea4159981be30e46b2dbe
Otavio <otavio.silva133@etec.sp.gov.br>
2020-05-24 22:23:08 -0300

    Initial Commit

---
+++ NeonUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeonUI
+//
+//  Created by Otavio de Souza on 5/24/20.
+//  Copyright © 2020 Otavio de Souza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
